07054da549b0438135c5c01d9610a533263cbe03
andylegrand <andyleg@uw.edu>
2023-06-20 10:55:55 -0700

    Initial Commit

---
+++ gptfood/gptfoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  gptfoodApp.swift
+//  gptfood
+//
+//  Created by Andy LeGrand on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct gptfoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
