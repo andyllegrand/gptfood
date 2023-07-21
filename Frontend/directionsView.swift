f895995e5b514a01f779aad2f037f642dd540f7d
andylegrand <andyleg@uw.edu>
2023-07-20 18:40:46 -0700

    7/20

---
+++ Frontend/directionsView.swift
@@ -0,0 +1,20 @@
+//
+//  directionsView.swift
+//  gptfood
+//
+//  Created by Andy LeGrand on 7/19/23.
+//
+
+import SwiftUI
+
+struct directionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct directionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        directionsView()
+    }
+}
